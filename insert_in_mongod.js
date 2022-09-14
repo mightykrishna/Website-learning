029f8e2429631e7ac57c6199b451b82d44d4da63
SAURABH KUMAR <94464793+saurabhpandeyjmp@users.noreply.github.com>
2022-09-15 02:08:26 +0600

    starter website learning

---
+++ insert_in_mongod.js
@@ -0,0 +1,3 @@
+use saurabhkart
+db.items.insertOne({name:"samsung",price:80000,rating:9,quality:"good"})
+db.items.insertmany([{name:"samsung",price:80000,rating:9,quality:"good",name:"samsung",price:80000,rating:9,quality:"good",name:"samsung",price:80000,rating:9,quality:"good",name:"samsung",price:80000,rating:9,quality:"good"}])
